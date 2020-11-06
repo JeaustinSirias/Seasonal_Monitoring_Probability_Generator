f3af6d433b7d1aaa7e073cfc33a498180ac27e02
JeaustinSirias <JEAUSTIN.SIRIAS@ucr.ac.cr>
2020-11-05 21:26:33 -0600

    adds unittesting and dockerfile

---
+++ test/unittest.py
@@ -0,0 +1,36 @@
+"""
+This file includes all the Unit Tests of the project.
+"""
+
+from unittest import TestCase
+from .context import core, utils
+
+
+class unittesting(TestCase):
+    """
+    Checks the functionality of the Calculator utility.
+    """
+
+    def import_test(self):
+        """
+        Checks the functionality of the addition.
+        """
+        assert 
+
+    def statistics_test(self):
+        """
+        Checks the functionality of the subtraction.
+        """
+        assert
+
+    def shapefile_test(self):
+        """
+        Checks the functionality of the multiplication.
+        """
+        assert 
+
+    def calcs(self):
+        """
+        Checks the functionality of the division.
+        """
+        assert 
